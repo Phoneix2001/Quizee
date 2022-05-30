6ae712f21db72c79d7c967befe65d3ab6c161a56
Arpit Babhoria <arpitkumar34774@gmail.com>
2022-05-30 08:12:55 +0530

    done some changes in activitymain5

--- app/src/main/java/com/kamikaze/practiceset3/MainActivity3.java
+++ app/src/main/java/com/kamikaze/practiceset3/MainActivity3.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
-
+public static final String thisisextra ="com.kamikaze.practiceset3.thisextraname";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
     }
     public void arpit(View view ){
+        EditText editText =findViewById(R.id.nomo);
         Intent intent = new Intent(this,MainActivity5.class);
+        intent.putExtra(thisisextra,editText.getText().toString());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/kamikaze/practiceset3/MainActivity5.java
+++ app/src/main/java/com/kamikaze/practiceset3/MainActivity5.java
@@ -1,12 +1,16 @@
 package com.kamikaze.practiceset3;
 
 
+import static com.kamikaze.practiceset3.MainActivity3.thisisextra;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -20,7 +24,10 @@ public class MainActivity5 extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
-
+        TextView textview =findViewById(R.id.textView10);
+        Intent intent =getIntent();
+        String message = intent.getStringExtra(thisisextra);
+        textview.setText("Congo ! your are my homie now : "+message);
 
     }
 
